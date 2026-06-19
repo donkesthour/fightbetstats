7b02833f9c033772c6f17f865990a8e1ae043054
steve <donkesthour@gmail.com>
2026-06-18 23:12:41 -0400

    Update UFC fight card and dashboard

---
+++ ufc-db.js
@@ -0,0 +1,283 @@
+﻿window.EMBEDDED_DATA = {
+    "bets":  [
+
+             ],
+    "events":  [
+                   {
+                       "id":  "event_muhammad_vs_bonfim",
+                       "name":  "Muhammad vs. Bonfim",
+                       "date":  "2026-06-06"
+                   },
+                   {
+                       "id":  "event_ufc_250",
+                       "name":  "UFC Freedom 250: Topuria vs. Gaethje",
+                       "date":  "2026-06-14"
+                   },
+                   {
+                       "id":  "event_kape_vs_horiguchi",
+                       "name":  "UFC Fight Night: Kape vs. Horiguchi",
+                       "date":  "2026-06-20"
+                   }
+               ],
+    "cards":  {
+                  "event_kape_vs_horiguchi":  [
+                                                  {
+                                                      "main":  true,
+                                                      "fighterA":  "Manel Kape",
+                                                      "fighterB":  "Kyoji Horiguchi",
+                                                      "weight":  "Flyweight",
+                                                      "oddsA":  -156,
+                                                      "oddsB":  135,
+                                                      "rounds":  5
+                                                  },
+                                                  {
+                                                      "main":  true,
+                                                      "fighterA":  "Navajo Stirling",
+                                                      "fighterB":  "Ion Cutelaba",
+                                                      "weight":  "Light Heavyweight",
+                                                      "oddsA":  -350,
+                                                      "oddsB":  275
+                                                  },
+                                                  {
+                                                      "main":  true,
+                                                      "fighterA":  "Vinicius Oliveira",
+                                                      "fighterB":  "Andre Fili",
+                                                      "weight":  "Featherweight",
+                                                      "oddsA":  -350,
+                                                      "oddsB":  285
+                                                  },
+                                                  {
+                                                      "main":  true,
+                                                      "fighterA":  "Christian Rodriguez",
+                                                      "fighterB":  "Hyder Amil",
+                                                      "weight":  "Featherweight",
+                                                      "oddsA":  -170,
+                                                      "oddsB":  140
+                                                  },
+                                                  {
+                                                      "main":  true,
+                                                      "fighterA":  "Murtazali Magomedov",
+                                                      "fighterB":  "Melsik Baghdasaryan",
+                                                      "weight":  "Featherweight",
+                                                      "oddsA":  -500,
+                                                      "oddsB":  385
+                                                  },
+                                                  {
+                                                      "main":  false,
+                                                      "fighterA":  "Andre Lima",
+                                                      "fighterB":  "Kevin Borjas",
+                                                      "weight":  "Flyweight",
+                                                      "oddsA":  -600,
+                                                      "oddsB":  450
+                                                  },
+                                                  {
+                                                      "main":  false,
+                                                      "fighterA":  "Karol Rosa",
+                                                      "fighterB":  "Luana Santos",
+                                                      "weight":  "Bantamweight",
+                                                      "oddsA":  -120,
+                                                      "oddsB":  100
+                                                  },
+                                                  {
+                                                      "main":  false,
+                                                      "fighterA":  "Bia Mesquita",
+                                                      "fighterB":  "Melissa Mullins",
+                                                      "weight":  "Bantamweight",
+                                                      "oddsA":  -550,
+                                                      "oddsB":  410
+                                                  },
+                                                  {
+                                                      "main":  false,
+                                                      "fighterA":  "Allan Nascimento",
+                                                      "fighterB":  "Mitch Raposo",
+                                                      "weight":  "Flyweight",
+                                                      "oddsA":  -220,
+                                                      "oddsB":  180
+                                                  },
+                                                  {
+                                                      "main":  false,
+                                                      "fighterA":  "Michael Aswell Jr.",
+                                                      "fighterB":  "Gaston Bolanos",
+                                                      "weight":  "Featherweight",
+                                                      "oddsA":  -300,
+                                                      "oddsB":  250
+                                                  },
+                                                  {
+                                                      "main":  false,
+                                                      "fighterA":  "Levan Chokheli",
+                                                      "fighterB":  "Leon Shahbazyan",
+                                                      "weight":  "Lightweight",
+                                                      "oddsA":  -310,
+                                                      "oddsB":  250
+                                                  },
+                                                  {
+                                                      "main":  false,
+                                                      "fighterA":  "Shane Collins",
+                                                      "fighterB":  "Otari Tanzilovi",
+                                                      "weight":  "Lightweight",
+                                                      "oddsA":  -175,
+                                                      "oddsB":  135
+                                                  }
+                                              ],
+                  "event_ufc_250":  [
+                                        {
+                                            "main":  true,
+                                            "fighterA":  "Ilia Topuria",
+                                            "fighterB":  "Justin Gaethje",
+                                            "weight":  "Lightweight",
+                                            "oddsA":  -750,
+                                            "oddsB":  425,
+                                            "rounds":  5
+                                        },
+                                        {
+                                            "main":  true,
+                                            "fighterA":  "Alex Pereira",
+                                            "fighterB":  "Ciryl Gane",
+                                            "weight":  "Heavyweight",
+                                            "oddsA":  -115,
+                                            "oddsB":  -105,
+                                            "rounds":  5
+                                        },
+                                        {
+                                            "main":  true,
+                                            "fighterA":  "Sean O\u0027Malley",
+                                            "fighterB":  "Aiemann Zahabi",
+                                            "weight":  "Bantamweight",
+                                            "oddsA":  -425,
+                                            "oddsB":  300
+                                        },
+                                        {
+                                            "main":  true,
+                                            "fighterA":  "Josh Hokit",
+                                            "fighterB":  "Derrick Lewis",
+                                            "weight":  "Heavyweight",
+                                            "oddsA":  -130,
+                                            "oddsB":  110
+                                        },
+                                        {
+                                            "main":  true,
+                                            "fighterA":  "Mauricio Ruffy",
+                                            "fighterB":  "Michael Chandler",
+                                            "weight":  "Lightweight",
+                                            "oddsA":  -650,
+                                            "oddsB":  425
+                                        },
+                                        {
+                                            "main":  true,
+                                            "fighterA":  "Bo Nickal",
+                                            "fighterB":  "Kyle Daukaus",
+                                            "weight":  "Middleweight",
+                                            "oddsA":  -370,
+                                            "oddsB":  270
+                                        },
+                                        {
+                                            "main":  false,
+                                            "fighterA":  "Diego Lopes",
+                                            "fighterB":  "Steve Garcia",
+                                            "weight":  "Featherweight",
+                                            "oddsA":  -190,
+                                            "oddsB":  145
+                                        }
+                                    ],
+                  "event_muhammad_vs_bonfim":  [
+                                                   {
+                                                       "main":  true,
+                                                       "fighterA":  "Belal Muhammad",
+                                                       "fighterB":  "Gabriel Bonfim",
+                                                       "weight":  "Welterweight",
+                                                       "oddsA":  -130,
+                                                       "oddsB":  100
+                                                   },
+                                                   {
+                                                       "main":  true,
+                                                       "fighterA":  "Brendan Allen",
+                                                       "fighterB":  "Edmen Shahbazyan",
+                                                       "weight":  "Middleweight",
+                                                       "oddsA":  -180,
+                                                       "oddsB":  150
+                                                   },
+                                                   {
+                                                       "main":  true,
+                                                       "fighterA":  "Fares Ziam",
+                                                       "fighterB":  "Tom Nolan",
+                                                       "weight":  "Lightweight",
+                                                       "oddsA":  100,
+                                                       "oddsB":  -120
+                                                   },
+                                                   {
+                                                       "main":  true,
+                                                       "fighterA":  "Bryce Mitchell",
+                                                       "fighterB":  "Santiago Luna",
+                                                       "weight":  "Bantamweight",
+                                                       "oddsA":  -250,
+                                                       "oddsB":  200
+                                                   },
+                                                   {
+                                                       "main":  true,
+                                                       "fighterA":  "Iwo Baraniewski",
+                                                       "fighterB":  "Junior Tafa",
+                                                       "weight":  "Light Heavyweight",
+                                                       "oddsA":  120,
+                                                       "oddsB":  -150
+                                                   },
+                                                   {
+                                                       "main":  false,
+                                                       "fighterA":  "Matt Schnell",
+                                                       "fighterB":  "Alessandro Costa",
+                                                       "weight":  "Catchweight",
+                                                       "oddsA":  150,
+                                                       "oddsB":  -180
+                                                   },
+                                                   {
+                                                       "main":  false,
+                                                       "fighterA":  "Marcus McGhee",
+                                                       "fighterB":  "John Yannis",
+                                                       "weight":  "Bantamweight",
+                                                       "oddsA":  -220,
+                                                       "oddsB":  180
+                                                   },
+                                                   {
+                                                       "main":  false,
+                                                       "fighterA":  "Bruno Silva",
+                                                       "fighterB":  "Edgar Chairez",
+                                                       "weight":  "Flyweight",
+                                                       "oddsA":  -140,
+                                                       "oddsB":  115
+                                                   },
+                                                   {
+                                                       "main":  false,
+                                                       "fighterA":  "Priscila Cachoeira",
+                                                       "fighterB":  "Chelsea Chandler",
+                                                       "weight":  "Bantamweight",
+                                                       "oddsA":  130,
+                                                       "oddsB":  -160
+                                                   },
+                                                   {
+                                                       "main":  false,
+                                                       "fighterA":  "Jordan Leavitt",
+                                                       "fighterB":  "Joanderson Brito",
+                                                       "weight":  "Featherweight",
+                                                       "oddsA":  220,
+                                                       "oddsB":  -280
+                                                   },
+                                                   {
+                                                       "main":  false,
+                                                       "fighterA":  "Jeisla Chaves",
+                                                       "fighterB":  "Yuneisy Duben",
+                                                       "weight":  "Flyweight",
+                                                       "oddsA":  110,
+                                                       "oddsB":  -130
+                                                   },
+                                                   {
+                                                       "main":  false,
+                                                       "fighterA":  "Ketlen Souza",
+                                                       "fighterB":  "Ariane Carnelossi",
+                                                       "weight":  "Strawweight",
+                                                       "oddsA":  -150,
+                                                       "oddsB":  125
+                                                   }
+                                               ]
+              },
+    "activeEventId":  "event_kape_vs_horiguchi",
+    "lastUpdated":  1781836462887
+};
